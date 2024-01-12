2a9fec3112d3787d2016fb30dd43b3d98d54ff26
Jonah Storch <jas1177@oit.rutgers.edu>
2024-01-12 00:33:11 -0500

    use package.json for package management

--- frontend/dev-server.sh
+++ frontend/dev-server.sh
@@ -1,9 +1,4 @@
-npm install uniqid \
-@babel/core @babel/preset-env @babel/preset-react @babel/plugin-transform-object-assign babel-loader url-loader \
-webpack webpack-cli html-webpack-plugin css-loader autoprefixer style-loader postcss-loader webpack-remove-debug copy-webpack-plugin \
-react react-dom react-router-dom \
-tailwindcss postcss react-date-range \
-js-cookie socket.io-client webpack-dev-server webpack-cli @headlessui/react @babel/preset-typescript
+npm install .
 
 npx tailwindcss -i ./input.css -o index.css
 
