93a32a67d2ed7a7524c5679fbed6000464574cc7
Jonah Storch <jas1177@oit.rutgers.edu>
2024-01-31 16:02:30 -0500

    fixed mongo commands for testing

--- testing/backend-testing/lib/mongodb.bash
+++ testing/backend-testing/lib/mongodb.bash
@@ -4,12 +4,6 @@ function create_user {
     done
 }
 
-function mongo_dump {
-    user_dump=$(mongosh meetme --quiet --eval 'db.users.find({})' | md5sum | cut -d ' ' -f1 & )
-    cal_main_dump=$(mongosh meetme --quiet --eval 'db.calendar_mains.find({})' | md5sum | cut -d ' ' -f1 & )
-    cal_meta_dump=$(mongosh meetme --quiet --eval 'db.calendar_metas.find({})' | md5sum | cut -d ' ' -f1 & )
-    cal_org_dump=$(mongosh meetme --quiet --eval 'db.organizations.find({})' | md5sum | cut -d ' ' -f1 & )
-    wait
-    
-    echo "${user_dump}${cal_main_dump}${cal_meta_dump}${cal_org_dump}" | md5sum | cut -d ' ' -f1
+function mongo_dump {    
+    echo "$( $($(mongodump --db=meetme --archive --excludeCollection=mongo_events)) | md5sum | cut -d ' ' -f1 )"
 }
--- testing/backend-testing/scripts/install_mongosh.sh
+++ testing/backend-testing/scripts/install_mongosh.sh
@@ -1,10 +1,23 @@
 distro="deb" #weird mongo stuff
-arch=$(uname -m)
+
+arch_1=$(uname -m)
+arch_2=$(dpkg --print-architecture)
+
 MONGO_TARGET=$(. /etc/os-release && echo "${ID}${VERSION_ID}")
 
+curl https://downloads.mongodb.com/compass/mongosh.json > response.json
 
-MONGOSH_INSTALL=$(curl https://downloads.mongodb.com/compass/mongosh.json | jq -r --arg arch "${arch}" --arg distro "${distro}" --arg target "${MONGO_TARGET}" \
-    '.versions[0].downloads[] | select (.arch==$arch and .distro==$distro and .targets[] == $target and .sharedOpenssl == null ) | .archive.url' )
+MONGOSH_INSTALL_URL=$(jq -r --arg arch "${arch_1}" --arg distro "${distro}" --arg target "${MONGO_TARGET}" \
+    '.versions[0].downloads[] | select (.arch==$arch and .distro==$distro and .targets[] == $target and .sharedOpenssl == null ) ' response.json)
 
-curl "$MONGOSH_INSTALL" -o mongosh.deb
+# .archive.url
+if [ -z "$MONGOSH_INSTALL_URL" ]]; then
+   MONGOSH_INSTALL_URL=$(jq -r --arg arch "${arch_2}" --arg distro "${distro}" --arg target "${MONGO_TARGET}" \
+    '.versions[0].downloads[] | select (.arch==$arch and .distro==$distro and .targets[] == $target and .sharedOpenssl == null )' response.json)
+fi
+
+MONGOSH_INSTALL_URL="$(jq -rn --argjson x "${MONGOSH_INSTALL_URL}" '$x.archive.url')"
+echo "installing .deb file from ${MONGOSH_INSTALL_URL}"
+
+curl "$MONGOSH_INSTALL_URL" -o mongosh.deb
 dpkg -i mongosh.deb
