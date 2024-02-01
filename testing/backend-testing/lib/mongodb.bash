1ba5d448b888469213abac99a34cda6efe52c9dc
Jonah Storch <jas1177@oit.rutgers.edu>
2024-02-01 17:47:41 -0500

    removed setting the date from create user

--- testing/backend-testing/lib/mongodb.bash
+++ testing/backend-testing/lib/mongodb.bash
@@ -3,7 +3,7 @@ function get_user {
 }
 
 function create_user {
-    mongosh meetme --quiet --eval "db.users.replaceOne({_id: '${1}'},$(jq -r "._id = \"${1}\" | .last_signin = -1" /templates/user.json),{upsert: true})"
+    mongosh meetme --quiet --eval "db.users.replaceOne({_id: '${1}'},$(jq -r "._id = \"${1}\"" /templates/user.json),{upsert: true})"
 }
 
 function mongo_dump {
