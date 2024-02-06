d6807a23a98be7b2a7263c679a026a3dda71be78
Andrew <as3842@oit.rutgers.edu>
2024-02-06 13:49:36 -0500

    fixed logout

--- backend/auth/logout.js
+++ backend/auth/logout.js
@@ -2,7 +2,10 @@ const { isAuthenticated } = require("./passport/util");
 
 module.exports = function (router, passport) {
   router.get('/logout',isAuthenticated, function (req, res, next) {
-    req.logout();
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
 
     res.json({
       Status: 'ok',
