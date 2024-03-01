d179221bed49fbce524f0740ce8e5ed96e9a6105
Jonah Storch <jas1177@oit.rutgers.edu>
2024-03-01 17:47:59 -0500

    final changes (hopefully)

--- testing/backend-testing/lib/mongodb.bash
+++ testing/backend-testing/lib/mongodb.bash
@@ -9,7 +9,7 @@ function create_user {
 function mongo_dump {
     temp_dir=$(mktemp -d)
     MONGO_URI='mongodb://localhost:27017/meetme?replicaSet=rs0'
-    if [ -z ${GITLAB_CI} ]; then
+    if [ -v GITLAB_CI ]; then
         MONGO_URI='mongodb://mongo:27017/meetme?replicaSet=rs0'
     fi
     echo "$( mongodump --db=meetme --uri "$MONGO_URI" --out $temp_dir --excludeCollection=mongo_events 2> /dev/null && find $temp_dir -type f -print | xargs md5sum | cut -d ' ' -f1 | md5sum | cut -d ' ' -f1)" || fail
