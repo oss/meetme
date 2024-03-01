e4511a703e17347d13f7b1757e54a4d3d2201fbe
Jonah Storch <jas1177@oit.rutgers.edu>
2024-03-01 17:56:12 -0500

    more mongo fixes

--- testing/backend-testing/lib/mongodb.bash
+++ testing/backend-testing/lib/mongodb.bash
@@ -1,8 +1,18 @@
 function get_user {
+    MONGO_URI='mongodb://localhost:27017/meetme?replicaSet=rs0'
+    if [ -v GITLAB_CI ]; then
+        MONGO_URI='mongodb://mongo:27017/meetme?replicaSet=rs0'
+    fi
+    
     mongosh meetme --quiet --eval "JSON.stringify(db.users.findOne({_id: '${1}'},{__v: 0}))" || fail
 }
 
 function create_user {
+    MONGO_URI='mongodb://localhost:27017/meetme?replicaSet=rs0'
+    if [ -v GITLAB_CI ]; then
+        MONGO_URI='mongodb://mongo:27017/meetme?replicaSet=rs0'
+    fi
+
     mongosh meetme --quiet --eval "db.users.replaceOne({_id: '${1}'},$(jq -r "._id = \"${1}\"" /templates/user.json),{upsert: true})" || fail
 }
 
