8f48c4789625a5f9304d935f78e93d20c88f5b54
Jonah Storch <jas1177@oit.rutgers.edu>
2024-03-01 17:57:33 -0500

    more mongo fixes

--- testing/backend-testing/lib/mongodb.bash
+++ testing/backend-testing/lib/mongodb.bash
@@ -3,8 +3,8 @@ function get_user {
     if [ -v GITLAB_CI ]; then
         MONGO_URI='mongodb://mongo:27017/meetme?replicaSet=rs0'
     fi
-    
-    mongosh meetme --quiet --eval "JSON.stringify(db.users.findOne({_id: '${1}'},{__v: 0}))" || fail
+
+    mongosh meetme --quiet  --uri "$MONGO_URI" --eval "JSON.stringify(db.users.findOne({_id: '${1}'},{__v: 0}))" || fail
 }
 
 function create_user {
@@ -13,7 +13,7 @@ function create_user {
         MONGO_URI='mongodb://mongo:27017/meetme?replicaSet=rs0'
     fi
 
-    mongosh meetme --quiet --eval "db.users.replaceOne({_id: '${1}'},$(jq -r "._id = \"${1}\"" /templates/user.json),{upsert: true})" || fail
+    mongosh meetme --quiet  --uri "$MONGO_URI" --eval "db.users.replaceOne({_id: '${1}'},$(jq -r "._id = \"${1}\"" /templates/user.json),{upsert: true})" || fail
 }
 
 function mongo_dump {
