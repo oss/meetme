50c94e5cd9907ad972669afee9391912c9149a6e
Jonah Storch <jas1177@oit.rutgers.edu>
2024-03-01 21:55:58 -0500

    move to mongo hashing with mongosh and dbHash

--- testing/backend-testing/lib/mongodb.bash
+++ testing/backend-testing/lib/mongodb.bash
@@ -13,16 +13,14 @@ function create_user {
         MONGO_URI='mongodb://mongo:27017/meetme'
     fi
 
-    mongosh meetme "$MONGO_URI" --quiet  --eval "db.users.replaceOne({_id: '${1}'},$(jq -r "._id = \"${1}\"" /templates/user.json),{upsert: true})" || fail
+    mongosh "$MONGO_URI" --quiet  --eval "db.users.replaceOne({_id: '${1}'},$(jq -r "._id = \"${1}\"" /templates/user.json),{upsert: true})" || fail
 }
 
 function mongo_dump {
-    temp_dir=$(mktemp -d)
     MONGO_URI='mongodb://localhost:27017/meetme'
     if [ -v GITLAB_CI ]; then
         MONGO_URI='mongodb://mongo:27017/meetme'
     fi
-    mongodump --db=meetme --uri "$MONGO_URI" --out $temp_dir --excludeCollection=mongo_events 2> /dev/null && find $temp_dir -type f -print | xargs md5sum | cut -d ' ' -f1 | md5sum | cut -d ' ' -f1  || fail
+    mongosh "$MONGO_URI" --quiet --eval 'JSON.stringify(db.runCommand( { dbHash: 1 } ))' | jq -r '.collections | del(.mongo_events)' || fail
     #| cut -d ' ' -f1 | md5sum | cut -d ' ' -f1
-    rm -rf $temp_dir
 }
