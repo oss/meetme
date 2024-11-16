9224c3db9fdf09f2a2342b2becc486c1047715bd
Andrew Somers <as3842@oit.rutgers.edu>
2024-11-16 17:38:16 -0500

    store

--- frontend/store/googleStore.js
+++ frontend/store/googleStore.js
@@ -1,29 +1,10 @@
 import { create } from 'zustand';
-import { produce } from "immer";
 
 const useStore = create(set => {
 
-    const fetchGoogleEmail= async () => {
+    const fetchGoogleVerified= async () => {
 
-        const resp = await fetch(process.env.API_URL + `/user/google_email`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        const resp_json = await resp.json();
-
-        if (resp_json.Status === 'ok') {
-            set( state => ({
-                googleEmail:resp_json.email
-            }))
-        }
-    }
-
-    const fetchGoogleLink= async () => {
-
-        const resp = await fetch(process.env.API_URL + `/user/google_auth_link`, {
+        const resp = await fetch(process.env.API_URL + `/user/google_verified`, {
             method: 'GET',
             credentials: 'include',
             headers: {
@@ -34,84 +15,17 @@ const useStore = create(set => {
 
         if (resp_json.Status === 'ok') {
             set( state => ({
-                googleLink:resp_json.link
+                googleVerified: resp_json.verified,
             }))
         }
     }
 
 
-    const fetchGoogleData = async (calendarID, start, end) => {
-
-        const time = new Date(start).toISOString()
-        const time2 = new Date(end ).toISOString()
-    
-        const resp = await fetch(`${process.env.API_URL}/user/google_cal_dates`,
-            {
-                method: "POST",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    minTime:time,
-                    maxTime:time2,
-                })
-            },
-        );
-        const resp_json = await resp.json();
-
-        if(resp_json.Status === 'ok'){
-            const googlestartend = resp_json.data.items.map((object, i) => {
-                const startDate = new Date(object.start.dateTime).valueOf();
-                const endDate = new Date(object.end.dateTime).valueOf();
-                return {start: startDate,  end: endDate};
-            })
-        
-        
-            const googleCals = [{_id: "netid1", times:googlestartend}]
-
-            set(
-                produce((prevState) => {
-                    prevState.googleData[calendarID] = {
-                        isLoaded: true,
-                        error: false,
-                        data: googleCals,
-                    };
-                }),
-            );
-        }
-    };
-
-    const addGoogleCalendar = async (calendarID) => {
-        set(
-            produce((prevState) => {
-                if (calendarID in prevState.googleData) {
-
-
-                    return;
-                }
-
-                prevState.googleData[calendarID] = {
-                    isLoaded: false,
-                    error: false,
-                    data: {},
-                };
-            }),
-        );
-
-    };
-
-
 
 
     return {
-        fetchGoogleEmail: fetchGoogleEmail,
-        fetchGoogleLink:fetchGoogleLink,
-        fetchGoogleData:fetchGoogleData,
-        addGoogleCalendar:addGoogleCalendar,
-        googleEmail:"",
-        googleLink:"",
-        googleData:{},
+        fetchGoogleVerified: fetchGoogleVerified,
+        googleVerified:false
     }
 })
 
