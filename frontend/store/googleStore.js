688155adbedf43736ba85d3066c1e3d09cbc45a8
Andrew Somers <as3842@oit.rutgers.edu>
2024-11-17 17:18:18 -0500

    cal works, need to make it work better and cleaner

--- frontend/store/googleStore.js
+++ frontend/store/googleStore.js
@@ -20,12 +20,57 @@ const useStore = create(set => {
         }
     }
 
+    const setGoogleCal = async () => {
+        const time = new Date().toISOString()
+        const time2 = new Date(Date.now() + 48 * (60 * 60 * 1000) ).toISOString()
+    
+        let data2 = await fetch(`${process.env.API_URL}/user/google_cal_dates`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                minTime:time,
+                maxTime:time2,
+            }),
+        }).then((res) => res.json());
+    
+        console.log(data2)
+    
+        const googlestartend = data2.data.items.map((object, i) => {
+            const startDate = new Date(object.start.dateTime).valueOf();
+            const endDate = new Date(object.end.dateTime).valueOf();
+            return {start: startDate,  end: endDate};
+        })
+    
+        console.log(googlestartend)
+    
+        const googleCals = {_id: "netid1", times:googlestartend}
+    
+        console.log(googleCals)
+
+        const arr = [googleCals]
+
+        console.log(arr)
+    
+        set((previous_state) => {
+
+            return {
+                googleCal: arr
+            }
+        })
+        
+    }
+
 
 
 
     return {
         fetchGoogleVerified: fetchGoogleVerified,
-        googleVerified:false
+        setGoogleCal:setGoogleCal,
+        googleVerified:false,
+        googleCal:[],
     }
 })
 
