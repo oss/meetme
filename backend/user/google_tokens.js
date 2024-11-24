00fe2cfa84d76ffb0b5a6ef3e45997ebcbb44347
Andrew Somers <as3842@oit.rutgers.edu>
2024-11-24 16:38:47 -0500

    making code better

--- backend/user/google_tokens.js
+++ backend/user/google_tokens.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User_schema = require('./user_schema');
-const Google_schema = require('./user_schema');
+const Google_schema = require('./google_state_schema');
 const mongoose = require('mongoose');
 const { isAuthenticated } = require('../auth/passport/util');
 const crypto =  require("crypto");
@@ -24,6 +24,8 @@ async function use_refresh_token(uid){
         grant_type:"refresh_token"
     }),
   }).then((res) => res.json());
+
+  console.log(data)
 
   const timeObject = new Date(Date.now() + data.expires_in * 1000);
 
@@ -49,7 +51,7 @@ async function newState(uid){
       return {
         Status: 'ok',
         Result: 'State added',
-        data: check,
+        state: google_state.state,
       };
       //adds user
     } catch (e) {
@@ -68,9 +70,46 @@ async function newState(uid){
     return {
       Status: 'ok',
       Result: 'State updated',
-      data: exists,
+      state: exists.state,
     };
   }
+}
+
+async function useCode(code, uid){
+  if (code === undefined || code === null) {
+    return 'error';
+  }
+
+  const data = await fetch(`https://oauth2.googleapis.com/token`, {
+    method: "POST",
+    credentials: "omit",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        code:code,
+        client_id:"35553104132-c9sos4lv16atkakg7t6nuoi9amktickk.apps.googleusercontent.com",
+        client_secret:"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",
+        redirect_uri:"https://localhost.edu",
+        grant_type:"authorization_code"
+    }),
+  }).then((res) => res.json());
+
+  console.log(data)
+
+  if (data?.error == "invalid_grant"){
+    return 'error'
+  }
+
+  const timeObject = new Date(Date.now() + data.expires_in * 1000);
+
+  //updates the userdata schema
+  const user_data = await User_schema.findOne({ _id: uid });
+  user_data.googleTokens.access_token = data.access_token;
+  user_data.googleTokens.refresh_token = data.refresh_token;
+  user_data.googleTokens.expires = timeObject.valueOf();
+  await user_data.save();
+  return 'ok';
 }
 
 router.patch('/google_tokens', isAuthenticated, async function (req, res) {
@@ -146,23 +185,32 @@ router.get('/google_info', isAuthenticated, async function (req, res) {
 
 
 router.get('/google_auth_link', isAuthenticated, async function (req, res) {
-  const STATE = "asifhalfi"
   const clientid = "35553104132-c9sos4lv16atkakg7t6nuoi9amktickk.apps.googleusercontent.com";
   const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
-  newState(req.user.uid);
+  const State_obj =  await newState(req.user.uid);
+
+  if  (State_obj.Status == 'error'){
+    res.json({
+      Status: 'error',
+      error: 'Something went wrong',
+    });
+    return;
+  }
+
+
 
   const user_data = await User_schema.findOne({ _id: req.user.uid });
-  user_data.googleTokens.state = STATE;
+  user_data.googleTokens.state = State_obj.state;
 
-  const link = `https://accounts.google.com/o/oauth2/v2/auth?scope=`+SCOPES+`&access_type=offline&include_granted_scopes=true&response_type=code&prompt=consent&state=`+STATE+`&redirect_uri=https://localhost.edu&client_id=`+clientid
+  const link = `https://accounts.google.com/o/oauth2/v2/auth?scope=`+SCOPES+`&access_type=offline&include_granted_scopes=true&response_type=code&prompt=consent&state=`+State_obj.state+`&redirect_uri=https://api.localhost.edu/user/code&client_id=`+clientid
 
   
   console.log(link)
 
   res.json({
     Status: 'ok',
-    location: link,
+    link: link,
   });
   return;
 });
@@ -171,7 +219,10 @@ router.post('/google_cal_dates', isAuthenticated, async function (req, res) {
 
   const user_data = await User_schema.findOne({ _id: req.user.uid });
 
-  if (user_data.googleTokens.access_token === undefined || user_data.googleTokens.access_toke === null) {
+  console.log("dates")
+  console.log(user_data)
+
+  if (user_data.googleTokens.access_token === '' || user_data.googleTokens.access_token === null) {
     res.json({
         Status: 'error',
         error: 'Not verified',
@@ -191,11 +242,15 @@ router.post('/google_cal_dates', isAuthenticated, async function (req, res) {
     return;
   }
 
-  use_refresh_token(req.user.uid);
-
-  if (new Date(user_data.googleTokens.expires) > new Date(Date.now() + 60 * 1000) ){
-    use_refresh_token(req.user.uid);
+  if (user_data.googleTokens.refresh_token === undefined || user_data.googleTokens.refresh_token === null) {
+    res.json({
+        Status: 'error',
+        error: 'Not verified',
+    });
+    return;
   }
+
+  //use_refresh_token(req.user.uid);
 
   const data = await fetch(`https://www.googleapis.com/calendar/v3/calendars/primary/events?access_token=${user_data.googleTokens.access_token}&singleEvents=True&orderBy=startTime&timeMin=${minTime}&timeMax=${maxTime}`, {
         method: "GET",
@@ -231,11 +286,11 @@ router.get('/code', isAuthenticated, async function (req, res) {
   console.log(req.query.code)
   console.log(req.query.state)
 
-  const user = await User_schema.findOne({ _id: req.user.uid });
-
   const state = await Google_schema.findOne({ _id: req.user.uid });
 
-  if (req.query.state != state.state){
+  console.log(state)
+
+  if (state == null || req.query.state != state.state){
     res.json({
       Status: 'error',
       error: 'Invalid request',
@@ -244,6 +299,10 @@ router.get('/code', isAuthenticated, async function (req, res) {
   }
 
   console.log("user")
+
+  await useCode(req.query.code, req.user.uid)
+
+  console.log("code used")
 
 
   res.redirect('https://localhost.edu');
