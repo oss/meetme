415e2f268c9193b0918f353752091b6d9d460730
Andrew Somers <as3842@oit.rutgers.edu>
2024-11-26 17:21:34 -0500

    fixed crashes

--- frontend/pages/dashboard/index.tsx
+++ frontend/pages/dashboard/index.tsx
@@ -255,14 +255,12 @@ function Dashboard() {
     const googleEmail = googleStore((store) => store.googleEmail);
     const fetchGoogleEmail = googleStore((store) => store.fetchGoogleEmail);
     const addGoogleCalendar = googleStore((store) => store.addGoogleCalendar);
-    const setGoogleCal = googleStore((store) => store.setGoogleCal);
 
     const time = new Date().toISOString()
     const time2 = new Date(Date.now() + 48 * (60 * 60 * 1000) ).toISOString()
 
     useEffect( ()=>{
         fetchGoogleEmail()
-        setGoogleCal()
         addGoogleCalendar("6uetvqHgDScEm8_82CLy5D-L1qyoKwxzev1t1UKvv606Eef6Zmx-yqw_Fqr2-JlISwvQHXd2j74rhce4USpb4Q", time, time2) 
     },[])
 
--- frontend/store/googleStore.js
+++ frontend/store/googleStore.js
@@ -21,49 +21,6 @@ const useStore = create(set => {
         }
     }
 
-    const setGoogleCal = async () => {
-        const time = new Date().toISOString()
-        const time2 = new Date(Date.now() + 48 * (60 * 60 * 1000) ).toISOString()
-    
-        let data2 = await fetch(`${process.env.API_URL}/user/google_cal_dates`, {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                minTime:time,
-                maxTime:time2,
-            }),
-        }).then((res) => res.json());
-
-        if (data2.Status == "ok"){
-            console.log(data2)
-            const googlestartend = data2.data.items.map((object, i) => {
-                const startDate = new Date(object.start.dateTime).valueOf();
-                const endDate = new Date(object.end.dateTime).valueOf();
-                return {start: startDate,  end: endDate};
-            })
-        
-            console.log(googlestartend)
-        
-            const googleCals = {_id: "netid1", times:googlestartend}
-        
-            console.log(googleCals)
-    
-            const arr = [googleCals]
-    
-            console.log(arr)
-        
-            set((previous_state) => {
-    
-                return {
-                    googleCal: arr
-                }
-            })
-        }
-
-    }
 
     const fetchGoogleData = async (calendarID, start, end) => {
 
@@ -84,8 +41,6 @@ const useStore = create(set => {
             },
         );
         const resp_json = await resp.json();
-        console.log("resp_json")
-        console.log(resp_json)
 
         if(resp_json.Status === 'ok'){
             const googlestartend = resp_json.data.items.map((object, i) => {
@@ -94,8 +49,6 @@ const useStore = create(set => {
                 return {start: startDate,  end: endDate};
             })
         
-            console.log("FETCH")
-            console.log(googlestartend)
         
             const googleCals = [{_id: "netid1", times:googlestartend}]
 
@@ -111,14 +64,11 @@ const useStore = create(set => {
         }
     };
 
-    const addGoogleCalendar = async (calendarID, start, end) => {
-        let shouldFetch = true;
-
+    const addGoogleCalendar = async (calendarID) => {
         set(
             produce((prevState) => {
                 if (calendarID in prevState.googleData) {
-                    //always update
-                    shouldFetch = true;
+
 
                     return;
                 }
@@ -131,12 +81,6 @@ const useStore = create(set => {
             }),
         );
 
-        console.log("startadd")
-        console.log(start)
-        console.log(end)
-
-        //if (!shouldFetch) return;
-        fetchGoogleData(calendarID, start, end);
     };
 
 
@@ -144,11 +88,9 @@ const useStore = create(set => {
 
     return {
         fetchGoogleEmail: fetchGoogleEmail,
-        setGoogleCal:setGoogleCal,
         fetchGoogleData:fetchGoogleData,
         addGoogleCalendar:addGoogleCalendar,
         googleEmail:"",
-        googleCal:[],
         googleData:{},
     }
 })
