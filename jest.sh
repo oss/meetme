9c19a756240e82dfad6b9752eb3047741b6827dc
Ivan Zhang <iz80@oit.rutgers.edu>
2025-04-11 17:23:14 -0400

    refactor(tests): pull frontend dependencies out of jest Dockerfile

--- jest.sh
+++ jest.sh
@@ -1,2 +1,2 @@
 docker build --file 'testing/frontend/jest/Dockerfile' -t meetme-jest 'testing/frontend/jest/.'
-docker run -v $(PWD):/root/work meetme-jest
+docker run -v $(pwd):/root/work meetme-jest -v $(pwd)/frontend/package.json:/root/work/frontend/package.json:ro
