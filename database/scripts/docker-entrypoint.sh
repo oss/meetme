cbf6c6eba18d2b74c4eaa202fac767652ced3d72
Jonah Storch <jas1177@oit.rutgers.edu>
2025-06-07 21:55:19 -0400

    Auto-create replica set if not created

--- database/scripts/docker-entrypoint.sh
+++ database/scripts/docker-entrypoint.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
 ./mongodb_exporter --mongodb.uri=mongodb://127.0.0.1:27017 --discovering-mode --compatible-mode=true --collect-all &
+
+rm /var/run/external/exporter.sock
 socat UNIX-LISTEN:/var/run/external/exporter.sock,fork TCP:127.0.0.1:9216 &
-mongod --bind_ip_all --replSet rs0
+
+./initrs.sh &
+
+exec mongod --bind_ip_all --replSet rs0
