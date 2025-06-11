8f43e7ce05054cb066468abdce4e174d3df9355f
Jonah Storch <jas1177@oit.rutgers.edu>
2025-06-11 00:17:14 -0400

    auto config for opensearch

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ $CR build 'proxy/.' -t nginx-oss-meetme || exit 1
 $CR build 'proxy-ssl-upgrade/.' -t nginx-ssl-oss-meetme || exit 1
 $CR build 'proxy-internal/.' -t nginx-internal-oss-meetme || exit 1
 
-
+$CR build 'opensearch/.' -t opensearch-meetme || exit 1
 $CR build 'proxy-docker/.' -t nginx-oss-docker || exit 1
 
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) || exit 1
