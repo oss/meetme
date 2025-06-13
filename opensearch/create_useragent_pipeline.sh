d76229840ffc2608ad2bf8b4c59db6092b80b605
Jonah Storch <jas1177@oit.rutgers.edu>
2025-06-12 22:58:23 -0400

    fix: verbose logs not sending

--- opensearch/create_useragent_pipeline.sh
+++ opensearch/create_useragent_pipeline.sh
@@ -11,16 +11,17 @@ wait_for_opensearch_boot() {
 
 wait_for_opensearch_boot;
 
-$CURL_CMD -H 'Content-Type: application/json' -X PUT https://localhost:9200/_ingest/pipeline/user_agent_pipeline --data '
-{
+
+$CURL_CMD -H 'Content-Type: application/json' -X PUT https://localhost:9200/_ingest/pipeline/user_agent_pipeline \
+    --data "$(jq -rn --arg allow_string 'ctx.user_agent != null' '{
   "description": "User agent pipeline",
   "processors": [
     {
       "user_agent": {
         "field": "user_agent",
-        "target_field": "user_agent_info"
+        "target_field": "user_agent_info",
+        "if": $allow_string
       }
     }
   ]
-}
-'
+}')" || kill -9 1
