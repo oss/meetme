78cdf1d4adb82412664728809aa8370b33f764eb
Jonah Storch <jas1177@oit.rutgers.edu>
2025-06-25 00:05:30 -0400

    frontend prod build w/ compose

--- build.sh
+++ build.sh
@@ -13,8 +13,8 @@ $CR build 'proxy-docker/.' -t nginx-oss-docker || exit 1
 
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) || exit 1
 GIT_HASH=$(git rev-parse HEAD) || exit 1
-$CR build 'backend/.' -t backend-meetme --build-arg GIT_BRANCH="$GIT_BRANCH" --build-arg GIT_HASH="$GIT_HASH" --build-arg DEV="true" --build-arg LOGIN_URL='https://idp.localhost.edu:4443/idp/profile/SAML2/Redirect/SSO' || exit 1
-$CR build 'frontend/.' -t frontend-meetme --build-arg BUILD="prod" || exit 1
+$CR build 'backend/.' -t backend-meetme --build-arg GIT_BRANCH="$GIT_BRANCH" --build-arg GIT_HASH="$GIT_HASH" || exit 1
+$CR build 'frontend/.' -t frontend-meetme || exit 1
 $CR build 'graphana/.' -t graphana-oss-meetme || exit 1
 $CR build 'database/.' -t database-meetme || exit 1
 $CR build 'websocket/.' -t websocket-meetme || exit 1
