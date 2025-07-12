f8bbd049c36c4b5ae4cd71256b8b711fa022c157
Ivan Zhang <iz80@oit.rutgers.edu>
2025-07-12 00:07:07 -0400

    fixup: start reverting stuff
    Things appear to be working now, reverting some commits

--- testing/backend/api/docker-entrypoint.sh
+++ testing/backend/api/docker-entrypoint.sh
@@ -1,6 +1,3 @@
-source ./lib/ldap.bash
-source ./lib/pupeteer.bash
-
 #THIS CODE IS FOR COOKIE JAR WHICH IS NO LONGER USED
 #Write cookie jars for curl
 touch /root/env
@@ -24,21 +21,19 @@ if [ $? -ne 0 ]; then
     echo -e "\e[31mError occured in preflight checks, double check tests to see what is wrong with setup\e[0m"
     exit 1;
 fi
-echo -e "\e[34mrunning tests now...\e[0m"
-NETID=$(create_ldap_user)
-echo "create ldap ${NETID}"
 
-COOKIE_NETID=$(getcookie "${NETID}")
-echo "got cookie ${COOKIE_NETID}"
-# $exe test/user.bats | tee "$TEST_OUTPUT_DIR/user.xml"
-# $exe test/ind_cal_creation.bats | tee "$TEST_OUTPUT_DIR/ind_cal_creation.xml"
-# $exe test/ind_cal_invites.bats | tee "$TEST_OUTPUT_DIR/ind_cal_invites.xml"
-# $exe test/ind_cal_memberlists.bats | tee "$TEST_OUTPUT_DIR/ind_cal_memberlists.xml"
-# $exe test/ind_cal_names.bats | tee "$TEST_OUTPUT_DIR/ind_cal_names.xml"
-#
-# $exe test/org.bats | tee "$TEST_OUTPUT_DIR/org.xml"
-# $exe test/org_cal.bats | tee "$TEST_OUTPUT_DIR/org_cal.xml"
-# $exe test/org_invites.bats | tee "$TEST_OUTPUT_DIR/org_invites.xml"
+echo -e "\e[34mrunning tests now...\e[0m"
+
+$exe test/user.bats | tee "$TEST_OUTPUT_DIR/user.xml"
+$exe test/ind_cal_creation.bats | tee "$TEST_OUTPUT_DIR/ind_cal_creation.xml"
+$exe test/ind_cal_invites.bats | tee "$TEST_OUTPUT_DIR/ind_cal_invites.xml"
+$exe test/ind_cal_memberlists.bats | tee "$TEST_OUTPUT_DIR/ind_cal_memberlists.xml"
+$exe test/ind_cal_names.bats | tee "$TEST_OUTPUT_DIR/ind_cal_names.xml"
+
+$exe test/org.bats | tee "$TEST_OUTPUT_DIR/org.xml"
+$exe test/org_cal.bats | tee "$TEST_OUTPUT_DIR/org_cal.xml"
+$exe test/org_invites.bats | tee "$TEST_OUTPUT_DIR/org_invites.xml"
+
 #bash print_env.sh
 if [ $? -ne 0 ]; then
     exit 1;
