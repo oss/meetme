57c27d77dc44eaf6ebc9e11b96a61c5ba3a2c1cd
Ivan Zhang <iz80@oit.rutgers.edu>
2025-08-13 13:43:24 -0400

    feat: log invalid and expired sessions

--- backend/auth/passport/util.js
+++ backend/auth/passport/util.js
@@ -7,7 +7,7 @@ function isAuthenticated(req, res, next) {
         const new_time = Math.floor( Date.now() / config.auth.session.update_unit );
         if(new_time - req.session.time >= config.auth.session.expiration){
 
-            traceLogger.verbose('session expired',req);
+            traceLogger.verbose('session expired',req, { netid: req.netid ?? "no netid" });
 
             req.session = null;
             res.json({
@@ -16,13 +16,13 @@ function isAuthenticated(req, res, next) {
             });
             return;
         }
-        traceLogger.verbose('session successfully authenticated',req);
+        traceLogger.verbose('session successfully authenticated', req, { netid: req.netid });
         if(new_time - req.session.time >= config.auth.session.update_interval)
             req.session.time = new_time;
 
         next();
     } else {
-        traceLogger.verbose('invalid or missing credential',req);
+        traceLogger.verbose('invalid or missing credential', req, { netid: req.netid ?? "no netid" });
         res.status(401).json({
             Status: 'error',
             error: 'Invalid or Missing Credentials',
