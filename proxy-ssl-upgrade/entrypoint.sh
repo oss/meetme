15c5327cafe7ca6b1efe83f0ad72e37bf210232a
ivanz <wispl.8qbkk@slmail.me>
2026-01-28 00:12:08 -0500

    feat: simplify proxy even more

--- proxy-ssl-upgrade/entrypoint.sh
+++ proxy-ssl-upgrade/entrypoint.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env sh
 
-envsubst '$SITE_URL' < /etc/nginx/templates/backend.conf > /etc/nginx/conf.d/backend.conf
+# Conditionally copy relevant nginx config files depending on the environment.
+# The difference between the environments is that the PROD=true environment has
+# no certs since our own proxy in front of it will handle SSL. For testing we
+# will default to using the certs.
+
+# TODO: right now, we still copy the certs in even with PROD=true, this
+# shouldn't really be too bad since it is just self-signed certs. But
+# we should consider deleting them from the container.
+
+backend="/etc/nginx/templates/backend.conf"
+frontend="/etc/nginx/templates/frontend.conf"
+
+if [ "$PROD" = "true" ]; then
+    backend="/etc/nginx/templates/backend-prod.conf"
+    frontend="/etc/nginx/templates/frontend-prod.conf"
+fi
+
+envsubst "$SITE_URL" < "$backend"  > /etc/nginx/conf.d/backend.conf
+cp "$frontend" /etc/nginx/conf.d/frontend.conf
+
 nginx -g "daemon off;"
