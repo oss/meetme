1f9fd37eaf165ae1676a0c4ced17ea11ebfb67c4
ivanz <wispl.8qbkk@slmail.me>
2026-06-09 19:40:11 -0400

    refactor: remove files, shib, saml

--- docs.sh
+++
@@ -1,8 +0,0 @@
-source ./rt.sh
-CR=$(check_runtime "$CR") || exit 1
-
-$CR build --file 'docs/Dockerfile' -t docs-meetme 'docs/.' || exit 1
-#$CR build --file 'jsdoc/Dockerfile' -t jsdoc-meetme 'jsdoc/.' || exit 1
-$CR run -v "$PWD/docs":"/root" -p 8000:8000 docs-meetme build
-$CR run -v "$PWD/docs":"/root" -p 8000:8000 docs-meetme serve -a 0.0.0.0:8000
-#$CR run -v "$PWD/backend":"/code" -v "$PWD/jsdoc/out":"/out" jsdoc-meetme
